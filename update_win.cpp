650b9c2de22e60b90e76418715f8dc500cbee0ba
Tomáš Garlík <tomas.garlik@seznam.cz>
2026-08-10 13:59:53 +0200

    update updated

--- update_win.cpp
+++ update_win.cpp
@@ -22,15 +22,25 @@ HWND hMainWnd = NULL;
 HWND hProgressBar = NULL;
 HWND hStatusText = NULL;
 std::atomic<bool> g_IsRunning(true);
-PROCESS_INFORMATION g_CurrentProcessInfo = {0};
+HANDLE g_hJob = NULL;
+
+void initJobObject() {
+    // Vytvoříme Job Object pro správu dceřiných procesů
+    g_hJob = CreateJobObjectA(NULL, NULL);
+    if (g_hJob) {
+        JOBOBJECT_EXTENDED_LIMIT_INFORMATION jeli = {0};
+        jeli.BasicLimitInformation.LimitFlags = JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE;
+        SetInformationJobObject(g_hJob, JobObjectExtendedLimitInformation, &jeli, sizeof(jeli));
+    }
+}
 
 void forceKillChildProcess() {
     g_IsRunning = false;
-    if (g_CurrentProcessInfo.hProcess != NULL) {
-        TerminateProcess(g_CurrentProcessInfo.hProcess, 0);
-        CloseHandle(g_CurrentProcessInfo.hProcess);
-        CloseHandle(g_CurrentProcessInfo.hThread);
-        g_CurrentProcessInfo.hProcess = NULL;
+    if (g_hJob != NULL) {
+        // Okamžitě ukončí všechny procesy v Jobu (cmd i PowerShell)
+        TerminateJobObject(g_hJob, 0);
+        CloseHandle(g_hJob);
+        g_hJob = NULL;
     }
 }
 
@@ -60,24 +70,29 @@ bool checkUnofficialUpdatesSetting(const std::string& configPath) {
 }
 
 bool executeHiddenCommand(const std::string& command) {
+    if (!g_IsRunning) return false;
+
     STARTUPINFOA si;
+    PROCESS_INFORMATION pi;
     ZeroMemory(&si, sizeof(si));
     si.cb = sizeof(si);
     si.dwFlags = STARTF_USESHOWWINDOW;
     si.wShowWindow = SW_HIDE;
-    
-    ZeroMemory(&g_CurrentProcessInfo, sizeof(g_CurrentProcessInfo));
+    ZeroMemory(&pi, sizeof(pi));
 
     std::string cmd = "cmd.exe /c " + command;
 
-    if (CreateProcessA(NULL, &cmd[0], NULL, NULL, FALSE, 0, NULL, NULL, &si, &g_CurrentProcessInfo)) {
-        WaitForSingleObject(g_CurrentProcessInfo.hProcess, INFINITE);
+    if (CreateProcessA(NULL, &cmd[0], NULL, NULL, FALSE, CREATE_BREAKAWAY_FROM_JOB, NULL, NULL, &si, &pi)) {
+        if (g_hJob) {
+            AssignProcessToJobObject(g_hJob, pi.hProcess);
+        }
+
+        WaitForSingleObject(pi.hProcess, INFINITE);
         DWORD exitCode = 1;
-        GetExitCodeProcess(g_CurrentProcessInfo.hProcess, &exitCode);
+        GetExitCodeProcess(pi.hProcess, &exitCode);
         
-        CloseHandle(g_CurrentProcessInfo.hProcess);
-        CloseHandle(g_CurrentProcessInfo.hThread);
-        g_CurrentProcessInfo.hProcess = NULL;
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
         
         return exitCode == 0;
     }
@@ -214,13 +229,11 @@ void asyncUpdateThread() {
     PostMessage(hMainWnd, WM_DESTROY, 0, 0);
 }
 
-// Obsluha správce okna
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     static int progressPos = 0;
 
     switch (msg) {
         case WM_CREATE:
-            // Časovač, který ručně posouvá progress bar každých 30 ms
             SetTimer(hwnd, 1, 30, NULL);
             return 0;
 
@@ -232,10 +245,9 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
             return 0;
 
         case WM_CLOSE:
-            // ZABIJEME VŠECHNY BEŽÍCÍ PROCESY A UKONČÍME APLIKACI OKAMŽITĚ
             forceKillChildProcess();
             DestroyWindow(hwnd);
-            ExitProcess(0); 
+            ExitProcess(0);
             return 0;
 
         case WM_DESTROY:
@@ -286,7 +298,6 @@ void createUpdateWindow(HINSTANCE hInstance) {
         hMainWnd, NULL, hInstance, NULL
     );
 
-    // Vytvořeno jako klasický Progress Bar s rozsahem 0-100
     hProgressBar = CreateWindowExA(
         0, PROGRESS_CLASS, NULL,
         WS_CHILD | WS_VISIBLE,
@@ -298,6 +309,7 @@ void createUpdateWindow(HINSTANCE hInstance) {
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    initJobObject();
     createUpdateWindow(hInstance);
 
     std::thread workerThread(asyncUpdateThread);
